129a237cd4bd57d5f8c3db39df5cf9b277558408
papavask <44700690+papavask@users.noreply.github.com>
2025-01-16 21:17:16 +0200

    Update play.py

--- play/play.py
+++ play/play.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # URL of the internet radio station (for example, a radio station's stream URL)
 radio_url = "https://stream.starfm.de/ballads/mp3-128/"  # Replace with the desired radio stream URL
-
+radio_url = "http://radiostreaming.ert.gr/ert-trito"
 # Display the title of the app
 st.title("Internet Radio Station Player")
 
