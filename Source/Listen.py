4831b628deb042d46402b1aa7abedbe8b97d2633
papavask <44700690+papavask@users.noreply.github.com>
2025-01-16 22:12:22 +0200

    Update Listen.py

--- Source/Listen.py
+++ Source/Listen.py
@@ -45,6 +45,8 @@ if st.button("Listen Music"):
     #st.write(type(Sradio_url))
     st.write(Sradio_url)
     st.write(len(Sradio_url), len(Sradio_url1))
+    for ii in range(len(Sradio_url)):
+        print(ord(Sradio_url[ii:ii]))
     if Sradio_url == Sradio_url1:
         st.write('!!!!!!!!!')
     
