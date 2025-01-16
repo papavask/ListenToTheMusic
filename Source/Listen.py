14e3a92c83f59dbc5fa73b48b33bc98504acd915
papavask <44700690+papavask@users.noreply.github.com>
2025-01-16 21:18:45 +0200

    Update Listen.py

--- Source/Listen.py
+++ Source/Listen.py
@@ -36,7 +36,8 @@ if st.button("Listen Music"):
     Radio_url_list = Radio_url_df.values.tolist()
     Radio_url = Radio_url_list[0][2]
     #st.write(selected_station +"\n\n")
-    st.write(Radio_url)
+    #st.write(Radio_url)
+    st.audio(Radio_url, format="audio/mp3")
     pass
 
 if st.button("Stop listening"):
