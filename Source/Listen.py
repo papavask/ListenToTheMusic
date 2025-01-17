b5cd6f7a4e67777eaa2cdd1a707dbbfbe263dc74
papavask <44700690+papavask@users.noreply.github.com>
2025-01-17 11:51:49 +0200

    Update Listen.py

--- Source/Listen.py
+++ Source/Listen.py
@@ -8,7 +8,6 @@ im = Image.open("./Source/favicon.ico")
 st.set_page_config(
     page_title="Listen On-Line Radio Stations",
     page_icon=im,
-    #page_icon="📊",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
@@ -17,15 +16,31 @@ st.set_page_config(
     }
 )
 
+st.markdown(
+    f"""
+    <style>
+    .reportview-container {
+        background: url("bg-img.jpj")
+    }
+    .sidebar .sidebar-content {
+        background: url("sb-img.jpj")
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+        
 # Set title
 st.title("On-Line Radio Stations")
 
 subheader = st.subheader("Have Fun with music\nInstructions:")
 
 # Set text
-st.write(
-    "Welcome to the On-Lne Radio portal!\nUse the dropdown menus below to select a station,\n\nand then click the button to start listening."
-)
+st.write("Welcome to the On-Lne Radio portal!")
+st.write("Use the dropdown menus to select a station,\n\nand then click the button to start listening.")
+
+# insert image
+#st.image("assets/picture.jpg")
 
 file_path = "./Source/RadioList.csv"
 
@@ -33,24 +48,14 @@ data = pd.read_csv(file_path, sep=",")
 
 station_list = list(data["Station"])
 
-    # Select company
+# Select station
 selected_station = st.sidebar.selectbox(
                    "Select a station from the list",
                    station_list
                    )
 
 if st.button("Listen Music"):
-    Radio_url_df = data[data["Station"] == selected_station]
-    Radio_url_list = Radio_url_df.values.tolist()
-    Radio_url = Radio_url_list[0][2]
-    Sradio_url = str(Radio_url)
-    Sradio_url = Sradio_url.replace(" ", "")
-    Sradio_url = Sradio_url[1:-1]
-    Sradio_url = data[data["Station"] == selected_station].values.tolist()[0][2][2:-1]
-
-    
-    st.audio(Sradio_url, format="audio/mp3", autoplay=True)
+    Radio_url = data[data["Station"] == selected_station].values.tolist()[0][2][2:-1]
+    st.audio(Radio_url, format="audio/mp3", autoplay=True)
     
 
-# insert image
-#st.image("assets/picture.jpg")
